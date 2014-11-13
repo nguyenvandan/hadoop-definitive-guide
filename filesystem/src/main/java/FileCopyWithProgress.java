501ead3adf6d40eb1e22ebad0e6e8db53d8e51ae
vanguyen <vanguyen@SW-FRMV-225873.emea.wdpr.disney.com>
2014-11-13 17:47:49 +0100

    Hadoop filesystem status test. [TODO] run in MacOS system

--- filesystem/src/main/java/FileCopyWithProgress.java
+++ filesystem/src/main/java/FileCopyWithProgress.java
@@ -1,10 +1,10 @@
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
@@ -18,11 +18,11 @@ public class FileCopyWithProgress {
 
 		Configuration conf = new Configuration();
 		FileSystem fs = FileSystem.get(URI.create(dst), conf);
-		OutputStream out = fs.create(new Path(dst), new Progressable() {
+		FSDataOutputStream out = fs.create(new Path(dst), new Progressable() {
 			public void progress() {
 				System.out.print(".");
 			}
-		});
+		});		
 		IOUtils.copyBytes(in, out, 4096, true);
 	}
 }
