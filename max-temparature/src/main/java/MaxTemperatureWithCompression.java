fd1e2e8e359f8fa540f2d984c040ec21245b0e7e
vanguyen <vanguyen@SW-FRMV-225873.emea.wdpr.disney.com>
2014-11-21 16:24:30 +0100

    hadoop-io

--- max-temparature/src/main/java/MaxTemperatureWithCompression.java
+++ max-temparature/src/main/java/MaxTemperatureWithCompression.java
@@ -1,4 +1,12 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MaxTemperatureWithCompression {
 	public static void main(String[] args) throws Exception {
@@ -9,7 +17,25 @@ public class MaxTemperatureWithCompression {
 			System.exit(-1);
 		}
 		
-		Job job = new Job();
+		Configuration conf = new Configuration();
+		conf.setBoolean("mapreduce.map.output.compress", true);
+		conf.setClass("mapreduce.map.output.compression.codec", GzipCodec.class, CompressionCodec.class);
+		Job job = new Job(conf);
 		job.setJarByClass(MaxTemperature.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileOutputFormat.setCompressOutput(job, true);
+		FileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);
+		
+		job.setMapperClass(MaxTemperatureMapper.class);
+		job.setCombinerClass(MaxTemperatureReducer.class);
+		job.setReducerClass(MaxTemperatureReducer.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
